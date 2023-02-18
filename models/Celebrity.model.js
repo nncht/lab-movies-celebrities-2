0396e17b245dd7327b611a70ebc1a4bbb20fee04
Nana Chuto <mail@nanachuto.com>
2023-02-18 16:41:59 +0100

    Iteration 2: done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,14 @@
 //  Add your code here
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const celebritySchema = new Schema({
+  name: { type: String, required: true },
+  occupation: { type: String, default: "unknown" },
+  catchPhrase: { type: String, default: "I am a celebrity." },
+});
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
+
+module.exports = celebrities;
